46cc6c84b8866574bc4e0c709e18d3bd588f9f5b
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-11-01 23:56:42 +0600

    Submited

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,4 +1,4 @@
-//  .env.local Using For Firebase Configuration Connect
+
 const firebaseConfig = {
     apiKey: "AIzaSyBq1-i362B9bOOuBuoEMccKz_ueFDYqADA",
     authDomain: "develop-tour.firebaseapp.com",
@@ -6,11 +6,5 @@ const firebaseConfig = {
     storageBucket: "develop-tour.appspot.com",
     messagingSenderId: "568982345378",
     appId: "1:568982345378:web:656da347fad55ba35de9fb"
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 export default firebaseConfig;
